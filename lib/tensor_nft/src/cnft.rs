4dd9b66b57ff5a4e07613f3454aaa3ee0a3e8909
Richard Wu <richardwu1997@gmail.com>
2023-11-15 20:49:28 -0500

    Added cnft args methods.

--- lib/tensor_nft/src/cnft.rs
+++ lib/tensor_nft/src/cnft.rs
@@ -9,7 +9,11 @@ use anchor_lang::{
     },
     InstructionData,
 };
-use mpl_bubblegum::{self, state::metaplex_adapter::Creator};
+use mpl_bubblegum::{
+    self,
+    state::metaplex_adapter::{Creator, MetadataArgs},
+    utils::get_asset_id,
+};
 
 pub struct TransferArgs<'a, 'info> {
     pub root: [u8; 32],
@@ -140,6 +144,87 @@ pub fn hash_creators(creators: &[Creator]) -> Result<[u8; 32]> {
             .as_ref(),
     )
     .to_bytes())
+}
+
+pub enum MetadataSrc {
+    Metadata(MetadataArgs),
+    DataHash(DataHashArgs),
+}
+
+pub struct DataHashArgs {
+    pub meta_hash: [u8; 32],
+    pub creator_shares: Vec<u8>,
+    pub creator_verified: Vec<bool>,
+    pub seller_fee_basis_points: u16,
+}
+
+pub struct MakeCnftArgs<'a, 'info> {
+    pub nonce: u64,
+    pub metadata_src: MetadataSrc,
+    pub merkle_tree: &'a AccountInfo<'info>,
+    pub creator_accounts: &'a [AccountInfo<'info>],
+}
+
+pub struct CnftArgs {
+    pub asset_id: Pubkey,
+    pub data_hash: [u8; 32],
+    pub creator_hash: [u8; 32],
+    pub creators: Vec<Creator>,
+}
+
+pub fn make_cnft_args(args: MakeCnftArgs) -> Result<CnftArgs> {
+    let MakeCnftArgs {
+        metadata_src,
+        creator_accounts,
+        nonce,
+        merkle_tree,
+    } = args;
+
+    // --------------------------------------- from bubblegum/process_mint_v1
+
+    let (data_hash, creator_hash, creators) = match metadata_src {
+        MetadataSrc::Metadata(mplex_metadata) => {
+            let creator_hash = hash_creators(&mplex_metadata.creators)?;
+            let metadata_args_hash = hashv(&[mplex_metadata.try_to_vec()?.as_slice()]);
+            let data_hash = hashv(&[
+                &metadata_args_hash.to_bytes(),
+                &mplex_metadata.seller_fee_basis_points.to_le_bytes(),
+            ])
+            .to_bytes();
+
+            (data_hash, creator_hash, mplex_metadata.creators)
+        }
+        MetadataSrc::DataHash(DataHashArgs {
+            meta_hash,
+            creator_shares,
+            creator_verified,
+            seller_fee_basis_points,
+        }) => {
+            // Verify seller fee basis points
+            let data_hash = hashv(&[&meta_hash, &seller_fee_basis_points.to_le_bytes()]).to_bytes();
+            // Verify creators
+            let creators = creator_accounts
+                .iter()
+                .zip(creator_shares.iter())
+                .zip(creator_verified.iter())
+                .map(|((c, s), v)| Creator {
+                    address: c.key(),
+                    verified: *v,
+                    share: *s,
+                })
+                .collect::<Vec<_>>();
+            let creator_hash = hash_creators(&creators)?;
+
+            (data_hash, creator_hash, creators)
+        }
+    };
+
+    Ok(CnftArgs {
+        asset_id: get_asset_id(&merkle_tree.key(), nonce),
+        data_hash,
+        creator_hash,
+        creators,
+    })
 }
 
 // TODO: cant get import to work, keeps asking for further imports from the crate
