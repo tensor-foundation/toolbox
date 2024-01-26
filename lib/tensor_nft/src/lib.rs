9e29914663e1096baa12baebdaa50f09c32b5635
Fernando Otero <febo@tensor.so>
2024-01-26 13:46:21 +0000

    Add validate mint helper (#8)

--- lib/tensor_nft/src/lib.rs
+++ lib/tensor_nft/src/lib.rs
@@ -2,8 +2,10 @@ pub mod cnft;
 pub mod common;
 pub mod error;
 pub mod pnft;
+pub mod token2022;
 
 pub use cnft::*;
 pub use common::*;
 pub use error::*;
 pub use pnft::*;
+pub use token2022::*;
---
+++ lib/tensor_nft/src/token2022.rs
@@ -0,0 +1,53 @@
+use anchor_lang::{
+    solana_program::{account_info::AccountInfo, msg, program_error::ProgramError, pubkey::Pubkey},
+    Result,
+};
+use anchor_spl::token_interface::spl_token_2022::{
+    extension::{metadata_pointer::MetadataPointer, BaseStateWithExtensions, StateWithExtensions},
+    state::Mint,
+};
+
+/// Validates a Token 2022 non-fungible mint account.
+///
+/// For non-fungibles assets, the validation consists of checking that the mint:
+/// - has no more than 1 supply
+/// - has 0 decimals
+/// - has no mint authority
+/// - `ExtensionType::MetadataPointer` is present and points to the mint account
+pub fn validate_mint_t22(mint_info: &AccountInfo) -> Result<()> {
+    let mint_data = &mint_info.data.borrow();
+    let mint = StateWithExtensions::<Mint>::unpack(mint_data)?;
+
+    if !mint.base.is_initialized {
+        msg!("Mint is not initialized");
+        return Err(ProgramError::UninitializedAccount.into());
+    }
+
+    if mint.base.decimals != 0 {
+        msg!("Mint decimals must be 0");
+        return Err(ProgramError::InvalidAccountData.into());
+    }
+
+    if mint.base.supply != 1 {
+        msg!("Mint supply must be 1");
+        return Err(ProgramError::InvalidAccountData.into());
+    }
+
+    if mint.base.mint_authority.is_some() {
+        msg!("Mint authority must be none");
+        return Err(ProgramError::InvalidAccountData.into());
+    }
+
+    if let Ok(extension) = mint.get_extension::<MetadataPointer>() {
+        let metadata_address: Option<Pubkey> = extension.metadata_address.into();
+        if metadata_address != Some(*mint_info.key) {
+            msg!("Metadata pointer extension: metadata address mismatch");
+            return Err(ProgramError::InvalidAccountData.into());
+        }
+    } else {
+        msg!("Missing metadata pointer extension");
+        return Err(ProgramError::InvalidAccountData.into());
+    }
+
+    Ok(())
+}
