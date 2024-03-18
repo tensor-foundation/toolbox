5b9dc1726a654dfc1ca17bcf284034f4deb91d88
Samuel Vanderwaal <samuel.vanderwaal@gmail.com>
2024-03-18 08:13:47 -0800

    use keccak hash function; match equality functionality of previous implementation for consistency

--- toolbox/src/hash.rs
+++ toolbox/src/hash.rs
@@ -1,9 +1,9 @@
-use anchor_lang::solana_program::blake3::hashv;
+use anchor_lang::solana_program::keccak::hashv;
 
 pub fn validate_proof(root: &[u8; 32], leaf: &[u8; 32], proof: &[[u8; 32]]) -> bool {
     let mut path = *leaf;
     proof.iter().for_each(|sibling| {
-        path = if path < *sibling {
+        path = if path <= *sibling {
             hashv(&[&path, sibling]).0
         } else {
             hashv(&[sibling, &path]).0
