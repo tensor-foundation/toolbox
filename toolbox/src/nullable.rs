d8c186fa9a518d4e0b0ce1f75b506404453457ba
Samuel Vanderwaal <samuel.vanderwaal@gmail.com>
2024-04-15 08:46:32 -0800

    add NONE impls

--- toolbox/src/nullable.rs
+++ toolbox/src/nullable.rs
@@ -1,7 +1,11 @@
 use anchor_lang::prelude::*;
 
+const DEFAULT_PUBKEY: Pubkey = Pubkey::new_from_array([0u8; 32]);
+
 /// Used for Brosh types that can have a `None` value.
 pub trait Nullable: AnchorSerialize + AnchorDeserialize {
+    const NONE: Self;
+
     /// Indicates if the value is `Some`.
     fn is_some(&self) -> bool;
 
@@ -15,7 +19,7 @@ pub trait Nullable: AnchorSerialize + AnchorDeserialize {
 /// This can be used when a specific value of `T` indicates that its
 /// value is `None`.
 #[repr(C)]
-#[derive(AnchorSerialize, AnchorDeserialize, Clone, Copy, Debug, Default, Eq, PartialEq, Hash)]
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Copy, Debug, Eq, PartialEq, Hash)]
 pub struct NullableOption<T: Nullable>(T);
 
 impl<T: Nullable> NullableOption<T> {
@@ -41,21 +45,36 @@ impl<T: Nullable> NullableOption<T> {
             None
         }
     }
+
+    #[inline]
+    pub fn none() -> Self {
+        Self(T::NONE)
+    }
+}
+
+impl<T: Nullable> Default for NullableOption<T> {
+    fn default() -> Self {
+        Self(T::NONE)
+    }
 }
 
 impl Nullable for Pubkey {
+    const NONE: Self = DEFAULT_PUBKEY;
+
     fn is_some(&self) -> bool {
-        self != &Pubkey::default()
+        self != &Self::NONE
     }
 
     fn is_none(&self) -> bool {
-        self == &Pubkey::default()
+        self == &Self::NONE
     }
 }
 
 macro_rules! impl_nullable_for_ux {
     ($ux:ty) => {
         impl Nullable for $ux {
+            const NONE: Self = 0;
+
             fn is_some(&self) -> bool {
                 *self != 0
             }
