89eb7b5a01fd3ac185ca3d089aff9e548acc2746
Samuel Vanderwaal <samuel.vanderwaal@gmail.com>
2024-04-15 14:52:13 -0800

    add NONE impls (#5)
    * add NONE impls
    
    * use Self::NONE for consistency
    
    * add tests & docs comments
    
    * update docs comments
    
    * fix pr request

--- toolbox/src/nullable.rs
+++ toolbox/src/nullable.rs
@@ -1,7 +1,12 @@
 use anchor_lang::prelude::*;
 
+const DEFAULT_PUBKEY: Pubkey = Pubkey::new_from_array([0u8; 32]);
+
 /// Used for Brosh types that can have a `None` value.
 pub trait Nullable: AnchorSerialize + AnchorDeserialize {
+    /// The value that represents `None`.
+    const NONE: Self;
+
     /// Indicates if the value is `Some`.
     fn is_some(&self) -> bool;
 
@@ -9,13 +14,15 @@ pub trait Nullable: AnchorSerialize + AnchorDeserialize {
     fn is_none(&self) -> bool;
 }
 
-/// A fixed-size Borsh type that can be used as an `Option<T>` without
-/// requiring extra space to indicate if the value is `Some` or `None`.
+/// Borsh encodes standard `Option`s with either a 1 or 0 representing the `Some` or `None variants.
+/// This means an `Option<Pubkey>` for example, is alternately encoded as 33 bytes or 1 byte.
+/// `NullableOption` type allows creating a fixed-size generic `Option` type that can be used as an `Option<T>` without
+/// requiring extra space to indicate if the value is `Some` or `None`. In the `Pubkey` example it is now
+/// always 32 bytes making it friendly to getProgramAccount calls and creating fixed-size on-chain accounts.
 ///
-/// This can be used when a specific value of `T` indicates that its
-/// value is `None`.
+/// This requires `T` to implement the `Nullable` trait so that it defines a `NONE` value and can indicate if it is `Some` or `None`.
 #[repr(C)]
-#[derive(AnchorSerialize, AnchorDeserialize, Clone, Copy, Debug, Default, Eq, PartialEq, Hash)]
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Copy, Debug, Eq, PartialEq, Hash)]
 pub struct NullableOption<T: Nullable>(T);
 
 impl<T: Nullable> NullableOption<T> {
@@ -41,27 +48,42 @@ impl<T: Nullable> NullableOption<T> {
             None
         }
     }
+
+    #[inline]
+    pub fn none() -> Self {
+        Self(T::NONE)
+    }
+}
+
+impl<T: Nullable> Default for NullableOption<T> {
+    fn default() -> Self {
+        Self::none()
+    }
 }
 
 impl Nullable for Pubkey {
+    const NONE: Self = DEFAULT_PUBKEY;
+
     fn is_some(&self) -> bool {
-        self != &Pubkey::default()
+        self != &Self::NONE
     }
 
     fn is_none(&self) -> bool {
-        self == &Pubkey::default()
+        self == &Self::NONE
     }
 }
 
 macro_rules! impl_nullable_for_ux {
     ($ux:ty) => {
         impl Nullable for $ux {
+            const NONE: Self = 0;
+
             fn is_some(&self) -> bool {
-                *self != 0
+                *self != Self::NONE
             }
 
             fn is_none(&self) -> bool {
-                *self == 0
+                *self == Self::NONE
             }
         }
     };
@@ -73,3 +95,48 @@ impl_nullable_for_ux!(u32);
 impl_nullable_for_ux!(u64);
 impl_nullable_for_ux!(u128);
 impl_nullable_for_ux!(usize);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_nullable_option() {
+        let mut none = NullableOption::<u8>::none();
+        assert!(none.value().is_none());
+        assert!(none.value_mut().is_none());
+
+        let mut some = NullableOption::new(42u8);
+        assert_eq!(some.value().unwrap(), &42);
+        assert_eq!(some.value_mut().unwrap(), &mut 42);
+
+        let opt = NullableOption::<Pubkey>::new(DEFAULT_PUBKEY);
+        assert!(opt.value().is_none());
+
+        let opt = NullableOption::<Pubkey>::new(Pubkey::new_from_array([1u8; 32]));
+        assert!(opt.value().is_some());
+        assert_eq!(opt.value().unwrap(), &Pubkey::new_from_array([1u8; 32]));
+    }
+
+    #[test]
+    fn test_nullable_pubkey() {
+        let none = Pubkey::NONE;
+        assert!(none.is_none());
+        assert!(!none.is_some());
+
+        let some = Pubkey::new_from_array([1u8; 32]);
+        assert!(!some.is_none());
+        assert!(some.is_some());
+    }
+
+    #[test]
+    fn test_nullable_ux() {
+        let none = 0u8;
+        assert!(none.is_none());
+        assert!(!none.is_some());
+
+        let some = 42u8;
+        assert!(!some.is_none());
+        assert!(some.is_some());
+    }
+}
