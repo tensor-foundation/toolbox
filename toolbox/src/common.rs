16d98bcf5098156a806aa2a318cfc73b0d021172
Samuel Vanderwaal <samv@tensor.so>
2024-06-28 12:18:38 -0800

    adjust calc_fees (#16)
    * adjust calc_fees
    
    * add total fee as a fn param
    
    * refactor to use u64s; add tnsr discount

--- toolbox/src/common.rs
+++ toolbox/src/common.rs
@@ -12,9 +12,12 @@ use vipers::prelude::*;
 
 use crate::TensorError;
 
-pub const HUNDRED_PCT_BPS: u16 = 10000;
-pub const GAMESHIFT_FEE_BPS: u16 = 200;
-pub const GAMESHIFT_BROKER_PCT: u16 = 50; // Out of 100
+pub const HUNDRED_PCT_BPS: u64 = 10000;
+pub const HUNDRED_PCT: u64 = 100;
+pub const GAMESHIFT_FEE_BPS: u64 = 200;
+pub const GAMESHIFT_BROKER_PCT: u64 = 50; // Out of 100
+pub const BROKER_FEE_PCT: u64 = 50;
+pub const TNSR_DISCOUNT_BP: u64 = 2500;
 
 pub const SINGLETONS: [Pubkey; 3] = [
     escrow::TSWAP_SINGLETON,
@@ -69,43 +72,60 @@ macro_rules! shard_num {
     };
 }
 
-pub fn calc_fees(
-    amount: u64,
-    fee_bps: u16,
-    maker_broker_pct: u16,
-    maker_broker: Option<Pubkey>,
-    _taker_broker: Option<Pubkey>,
-) -> Result<(u64, u64, u64)> {
-    let (fee_bps, maker_broker_pct) = if maker_broker == Some(crate::gameshift::ID) {
-        // gameshift fee schedule
-        (GAMESHIFT_FEE_BPS, GAMESHIFT_BROKER_PCT)
+pub struct CalcFeesArgs {
+    pub amount: u64,
+    pub total_fee_bps: u64,
+    pub broker_fee_pct: u64,
+    pub maker_broker_pct: u64,
+    pub tnsr_discount: bool,
+}
+
+pub fn calc_fees(args: CalcFeesArgs) -> Result<(u64, u64, u64)> {
+    let CalcFeesArgs {
+        amount,
+        total_fee_bps,
+        broker_fee_pct,
+        maker_broker_pct,
+        tnsr_discount,
+    } = args;
+
+    // Apply the TNSR discount if enabled.
+    let total_fee_bps = if tnsr_discount {
+        unwrap_checked!({
+            total_fee_bps
+                .checked_mul(HUNDRED_PCT_BPS - TNSR_DISCOUNT_BP)?
+                .checked_div(HUNDRED_PCT_BPS)
+        })
     } else {
-        (fee_bps, maker_broker_pct)
+        total_fee_bps
     };
 
-    let full_fee = unwrap_checked!({
-        (fee_bps as u64)
-            .checked_mul(amount)?
-            .checked_div(HUNDRED_PCT_BPS as u64)
-    });
-    let taker_broker_fee = 0; // todo: taker broker not enabled
-    let maker_broker_fee = unwrap_checked!({
-        full_fee
-            .checked_mul(maker_broker_pct as u64)?
-            .checked_div(100)
-    });
-    let protocol_fee = unwrap_checked!({
-        full_fee
-            .checked_sub(maker_broker_fee)
-            .unwrap()
-            .checked_sub(taker_broker_fee)
+    // Total fee is calculated from the passed in total_fee_bps and is protocol fee + broker fees.
+    let total_fee = unwrap_checked!({
+        (amount)
+            .checked_mul(total_fee_bps)?
+            .checked_div(HUNDRED_PCT_BPS)
     });
 
-    // Stupidity check, broker should never be higher than main fee (== when zero)
-    require!(
-        protocol_fee >= maker_broker_fee + taker_broker_fee,
-        TensorError::ArithmeticError
-    );
+    // Broker fees are a percentage of the total fee.
+    let broker_fees = unwrap_checked!({
+        total_fee
+            .checked_mul(broker_fee_pct)?
+            .checked_div(HUNDRED_PCT)
+    });
+
+    // Protocol fee is the remainder.
+    let protocol_fee = unwrap_checked!({ total_fee.checked_sub(broker_fees) });
+
+    // Maker broker is a percentage of the total brokers fee.
+    let maker_broker_fee = unwrap_checked!({
+        broker_fees
+            .checked_mul(maker_broker_pct)?
+            .checked_div(HUNDRED_PCT)
+    });
+
+    // Remaining broker fee is the taker broker fee.
+    let taker_broker_fee = unwrap_int!(broker_fees.checked_sub(maker_broker_fee));
 
     Ok((protocol_fee, maker_broker_fee, taker_broker_fee))
 }
@@ -139,7 +159,7 @@ pub fn calc_creators_fee(
     let fee = unwrap_checked!({
         creator_fee_bps
             .checked_mul(amount)?
-            .checked_div(HUNDRED_PCT_BPS as u64)
+            .checked_div(HUNDRED_PCT_BPS)
     });
 
     Ok(fee)
