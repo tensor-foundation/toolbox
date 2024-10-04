04b14cb25218efb515e940561431e3a295ede93c
Richard Wu <10563314+richardwu@users.noreply.github.com>
2024-10-04 11:04:09 -0400

    Revert error code change + commented out cfg.

--- toolbox/src/error.rs
+++ toolbox/src/error.rs
@@ -1,23 +1,23 @@
 use anchor_lang::prelude::*;
 
-#[error_code(offset = 9001)]
+#[error_code]
 pub enum TensorError {
     #[msg("bad royalties")]
-    BadRoyaltiesPct,
+    BadRoyaltiesPct = 9001,
     #[msg("insufficient balance")]
-    InsufficientBalance,
+    InsufficientBalance = 9002,
     #[msg("creator mismatch")]
-    CreatorMismatch,
+    CreatorMismatch = 9003,
     #[msg("failed leaf verification")]
-    FailedLeafVerification,
+    FailedLeafVerification = 9004,
     #[msg("arithmetic error")]
-    ArithmeticError,
+    ArithmeticError = 9005,
     #[msg("metadata account does not match")]
-    BadMetadata,
+    BadMetadata = 9006,
     #[msg("rule set for programmable nft does not match")]
-    BadRuleSet,
+    BadRuleSet = 9007,
     #[msg("invalid core asset")]
-    InvalidCoreAsset,
+    InvalidCoreAsset = 9008,
     #[msg("invalid fee account")]
-    InvalidFeeAccount,
+    InvalidFeeAccount = 9009,
 }
