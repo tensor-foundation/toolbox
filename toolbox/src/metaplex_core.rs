9161c9f360664ab84860f7dd86656cae49fd9f46
Samuel Vanderwaal <samv@tensor.so>
2024-10-26 14:34:11 -0800

    add validate_core_asset fn

--- toolbox/src/metaplex_core.rs
+++ toolbox/src/metaplex_core.rs
@@ -2,9 +2,14 @@ use anchor_lang::solana_program::account_info::AccountInfo;
 use anchor_lang::solana_program::msg;
 use anchor_lang::solana_program::pubkey::Pubkey;
 use anchor_lang::Result;
-use mpl_core::accounts::{BaseAssetV1, BaseCollectionV1};
-use mpl_core::fetch_plugin;
-use mpl_core::types::{Key, PluginType, Royalties, UpdateAuthority};
+use mpl_core::{
+    accounts::{BaseAssetV1, BaseCollectionV1},
+    fetch_plugin,
+    types::{
+        Creator, Key, PluginType, Royalties, UpdateAuthority, VerifiedCreators,
+        VerifiedCreatorsSignature,
+    },
+};
 
 use crate::TensorError;
 
@@ -15,6 +20,82 @@ impl anchor_lang::Id for MetaplexCore {
     fn id() -> Pubkey {
         mpl_core::ID
     }
+}
+
+pub struct CoreAsset {
+    pub collection: Option<Pubkey>,
+    pub whitelist_creators: Option<Vec<VerifiedCreatorsSignature>>,
+    pub royalty_creators: Option<Vec<Creator>>,
+    pub royalty_fee_bps: u16,
+}
+
+pub fn validate_core_asset(
+    asset_info: &AccountInfo,
+    maybe_collection_info: Option<&AccountInfo>,
+) -> Result<CoreAsset> {
+    // validate the asset account
+    assert_ownership(asset_info, Key::AssetV1)?;
+
+    // validates the collection is owned by the MPL Core program
+    maybe_collection_info
+        .as_ref()
+        .map(|c| assert_ownership(c, Key::CollectionV1));
+
+    let asset = BaseAssetV1::try_from(asset_info)?;
+
+    // if the asset has a collection, we must validate it, and fetch the royalties from it
+    let (mut royalties, collection) =
+        if let UpdateAuthority::Collection(asset_collection) = asset.update_authority {
+            // Collection account must be provided.
+            if maybe_collection_info.is_none() {
+                msg!("Asset has a collection but no collection account was provided");
+                return Err(TensorError::InvalidCoreAsset.into());
+            }
+
+            let collection_info = maybe_collection_info.unwrap();
+
+            // Collection account must match the one on the asset.
+            if asset_collection != *collection_info.key {
+                msg!("Asset collection account does not match the provided collection account");
+                return Err(TensorError::InvalidCoreAsset.into());
+            }
+
+            (
+                fetch_plugin::<BaseAssetV1, Royalties>(asset_info, PluginType::Royalties)
+                    .ok()
+                    .map(|(_, royalties, _)| royalties),
+                Some(asset_collection),
+            )
+        } else {
+            (None, None)
+        };
+
+    // Get royalties plugin from the asset, if present.
+    // Royalties plugin directly on the asset take precedence over the one on the collection.
+    royalties = fetch_plugin::<BaseAssetV1, Royalties>(asset_info, PluginType::Royalties)
+        .ok()
+        .map(|(_, royalties, _)| royalties)
+        .or(royalties);
+
+    let royalty_fee_bps = if let Some(Royalties { basis_points, .. }) = royalties {
+        basis_points
+    } else {
+        0
+    };
+
+    // Fetch the verified creators from the MPL Core asset and map into the expected type
+    // for whitelist verification.
+    let verified_creators: Option<Vec<VerifiedCreatorsSignature>> =
+        fetch_plugin::<BaseAssetV1, VerifiedCreators>(asset_info, PluginType::VerifiedCreators)
+            .map(|(_, verified_creators, _)| verified_creators.signatures)
+            .ok();
+
+    Ok(CoreAsset {
+        collection,
+        whitelist_creators: verified_creators,
+        royalty_creators: royalties.map(|r| r.creators),
+        royalty_fee_bps,
+    })
 }
 
 /// Validates a mpl-core asset.
@@ -68,7 +149,7 @@ pub fn validate_asset(
 }
 
 #[inline(always)]
-fn assert_ownership(account: &AccountInfo, discriminator: Key) -> Result<()> {
+pub fn assert_ownership(account: &AccountInfo, discriminator: Key) -> Result<()> {
     if account.owner != &mpl_core::ID {
         return Err(TensorError::InvalidCoreAsset.into());
     }
